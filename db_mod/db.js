09db8bc4f3dd15a986b71e48054b8aa2f46865ac
Agnieszka Konefał <68051717+koneweczka@users.noreply.github.com>
2022-08-02 10:13:54 +0200

    Update db.js

--- db_mod/db.js
+++ db_mod/db.js
@@ -1,31 +1,21 @@
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
-// ta chyba najlepsza:
 async function openDB() {
     // open the database
     const db = await open({
       filename: '/tmp/database.db',
       driver: sqlite3.Database
     });
-    // PRIMARY KEY - okresla, ze to jest klucz główny tablicy; a AUTOINCREMENT odpowiada za to, ze przy insercie jak wstawiasz nulla
-    // to on sam okresla nastepna wartosc poprawny i teraz nie potrzebuje tej logiki na ID z kodu
-    // nie ma stringa samego, jest text
+
     await db.exec('CREATE TABLE IF NOT EXISTS myTable (id INTEGER PRIMARY KEY AUTOINCREMENT, summary TEXT not null)')
     // await db.exec('INSERT INTO myTable(id, summary) VALUES(1, "Wynieść śmieci.")')
     return db
 };
 
-// To jest do listy:
-// let list = [
-//     {id: 0, summary: "Posprzątać kuchnię."},
-//     {id: 1, summary: "Wynieść śmieci."}
-// ];
-
-
 // to nie dziala jak definiuje funkcje z function, czemu?
 exports.showList = async () => {
-    // to sie owiera z pol. do db
+    // to otwiera z pol. do db
     const db = await openDB()
     try {
         const result = await db.all('SELECT * FROM myTable')
@@ -42,7 +32,6 @@ exports.showList = async () => {
 exports.addTask = async (summary) => {
         const db = await openDB()
         try {
-            // spr czy nie bylo """ w JSie"
             // jak dama tu nulla to bedzie autoincrement
             await db.run('INSERT INTO myTable(summary) VALUES($summary)', {
                 '$summary': summary
@@ -82,7 +71,3 @@ exports.removeTask = async (id) => {
         await db.close()
     }
 };
-
-// Tego nie wiem, czy potrzebuję:
-// tego nie musze miec, bo nie chce tego pokazywac:
-//exports.list = list;
